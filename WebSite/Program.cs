e6a27a031457df785c5d1f1dcfb9ec859b1d8c21
gurkan <batingurkan01@gmail.com>
2023-11-06 01:53:37 +0300

    fix

--- WebSite/Program.cs
+++ WebSite/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WebSite;
 
 var builder = WebApplication.CreateBuilder(args);
